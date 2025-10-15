4b30b81e581b79c77ebfd21a249ec7a9e7939cd4
Mathaus <mathausdev@gmail.com>
2025-10-15 17:27:26 -0300

    att program.css automatização

--- Program.cs
+++ Program.cs
@@ -27,6 +27,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+// 🟢 Aplicar migrações automaticamente ao iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
